bbc1a5a3376a24a63f446c21a4389b574d662d6c
sarahzxwy <sarahalvesborges@outlook.com>
2025-12-01 14:48:57 -0300

    feat: bot configuration and execution script

---
+++ robo.py
@@ -0,0 +1,24 @@
+from chatterbot import ChatBot
+
+NOME_ROBO = "COFFICERBot"
+CONFIANCA_MINIMA = 0.60
+
+def configurar_robo():
+    robo = ChatBot(NOME_ROBO, read_only = True)
+
+    return robo
+
+def executar_robo(robo):
+    while True:
+        mensagem = input("👤 ")
+        resposta = robo.get_response(mensagem.lower())
+        if resposta.confidence >= CONFIANCA_MINIMA:
+            print(f"🤖 {resposta.text} [confiança = {resposta.confidence}]")
+        else:
+            print(f"🤖 Ainda não sei responder isso. Tente outra pergunta. [confiança = {resposta.confidence}]")
+
+if __name__ == "__main__":
+    robo = configurar_robo()
+
+    if robo:
+        executar_robo(robo)
